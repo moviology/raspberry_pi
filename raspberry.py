bed3c4e5547179dee1490680f54f7123c022d659
conor <conorbegley5@gmail.com>
2022-12-04 16:20:38 +0000

    added a MySubsciberCallback class to the raspberry.py file

--- raspberry.py
+++ raspberry.py
@@ -4,6 +4,8 @@ import os
 from dotenv import load_dotenv
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
+from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNOperationType, PNStatusCategory
 
 load_dotenv()
 
@@ -21,22 +23,28 @@ arduino = serial.Serial(port='/dev/ACM0', baudrate=57600, timeout=.1)
 current_second = 0
 
 
+class MySubscribeCallback(SubscribeCallback):
+    def message(self, pubnub, message):
+        while message.message != "stop":
+            byte = arduino.readline()
+            decoded_bytes = byte.decode("utf-8")
+
+            if not decoded_bytes == "":
+                data_str = decoded_bytes.strip()
+                data_tokens = data_str.split(', ')
+
+                pub_list = list(map(int, data_tokens))
+                pub_list.append(current_second)
+                pub_list.append(MACHINE_ID)
+
+                current_second = current_second + 2
+                pubnub.publish().channel(PUBNUB_CHANNEL).message(pub_list).pn_async(publish_callback)
+
+
+pubnub.add_listener(MySubscribeCallback())
+
+
 def publish_callback(_, status):
     if status.is_error():
         print("ERROR: failed publishing at {}s".format(current_second))
 
-
-while True:
-    byte = arduino.readline()
-    decoded_bytes = byte.decode("utf-8")
-
-    if not decoded_bytes == "":
-        data_str = decoded_bytes.strip()
-        data_tokens = data_str.split(', ')
-
-        pub_list = list(map(int, data_tokens))
-        pub_list.append(current_second)
-        pub_list.append(MACHINE_ID)
-
-        current_second = current_second + 2
-        pubnub.publish().channel(PUBNUB_CHANNEL).message(pub_list).pn_async(publish_callback)
